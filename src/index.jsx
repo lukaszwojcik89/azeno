653cbe0ebc1253ead69375c22b684289f2e4726e
Łukasz Wójcik <wojcik.l@tlen.pl>
2022-05-21 11:36:21 +0200

    add support for scss and boostrap-react

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode } from 'react';
+import React, { StrictMode } from 'react';
+
 import App from './App';
 
 const root = createRoot(document.getElementById('app'));
