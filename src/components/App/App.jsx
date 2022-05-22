ccff5f64500e41223391f2363ecff10705965bde
Łukasz Wójcik <wojcik.l@tlen.pl>
2022-05-22 09:39:34 +0200

    Card Components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Login from '../../Auth/Login';
+import Register from '../../Auth/Register';
 import useCookie from '../../Hooks/useCookie';
-import Core from '../core/Core';
 import Header from '../Header/Header';
+import Profile from '../Profile/Profile';
+import Train from '../Train/Train';
 import './App.scss';
 
 function App() {
@@ -11,7 +14,13 @@ function App() {
     return (
         <BrowserRouter>
             <Header />
-            <Core cookie={cookie} />
+            <Routes>
+                <Route path="/" element={<Link to="/profile">Profile</Link>} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/train" element={<Train />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
         </BrowserRouter>
     );
 }
